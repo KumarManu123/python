3f4346e3bf0a7884b5aab6f2e306005f10926c01
KumarManu123 <kumarbhukya2012@gmail.com>
2024-08-07 17:23:49 +0530

    first commit

---
+++ rang.py
@@ -0,0 +1,53 @@
+# for i in range(10):
+#     print(i)
+
+# for value in range (12,152):
+#     print(value)
+
+# for value in range(0,10,1):
+#     print(value)
+
+# for i in range(0,1000):
+#     print(i)
+
+# import calendar
+# print(calendar.calendar(2024))
+
+# import calendar as cal
+# s = cal.TextCalendar()
+# Month = s.formatmonth(2024,2)
+# print(Month)
+
+# import datetime as dt
+# print(dt.datetime.now())
+
+# import datetime as dt
+# s = dt.datetime.now()
+# day = s.strftime('%A')
+# print(day)
+
+# import datetime
+# dob_date = '1997-09-06'
+# print(dob_date)
+# s = datetime.datetime.strptime(dob_date,'%Y-%m-%d')
+# dob_day = s.strftime('%A')
+# print(dob_day)
+
+# l = [2,5,8,1,3,6,4]
+# s = 0
+# for i in l:
+#     s = s+i
+#     print(s)
+
+# d = {'HYD':'NIMS','WGL':'MGM','HNK':'LIFE LINE'}
+# for k,v in d.items():
+#     print({k},':',{v})
+
+l = [1,2,3,4,5,5]
+print(l,type(l))
+
+t = (1,2,3,4,5,5)
+print(t,type(t))
+
+t2= 1,2,3
+print(t2,type(t2))
