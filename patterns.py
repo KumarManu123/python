86c1bbafe01bc3b25a0f58f15b6c22580668cb44
KumarManu123 <kumarbhukya2012@gmail.com>
2024-08-07 17:32:46 +0530

    first commit

---
+++ patterns.py
@@ -0,0 +1,25 @@
+
+# def center_try_angle(n):
+#     for i in range(n+1):
+#         print(' '*(n-i)+'*'*(2*i-1))
+# number=15
+# center_try_angle(number)
+
+
+def center_try_angle(n):
+    for i in range(n+1):
+        print(' '*(n-i)+'*'*(2*i-1))
+num=12
+center_try_angle(num)
+
+
+n=int(input("Enter is an prime Or not Prime num:"))
+if n>1=0:
+    for i in range(n):
+        if n%2==1:
+            print(f"{n} Is an prime num")
+            break
+    else:
+            print(f"{n} Is not an prime num")
+
+
